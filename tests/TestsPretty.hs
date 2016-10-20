36de87ce7d6a7d0d16d8f25e5314801749d879d6
Andy Balaam <andybalaam@artificialworlds.net>
2016-10-20 08:23:53 +0100

    First test of a simple roundtrip.

--- tests/TestsPretty.hs
+++ tests/TestsPretty.hs
@@ -1,16 +1,28 @@
 module Main where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty(defaultMain, testGroup)
+import Test.Tasty.HUnit(assertEqual, testCase)
 
---import Language.Java.Parser
---import Language.Java.Syntax
---import Language.Java.Pretty
+import Control.Applicative
+import Language.Java.Parser(parser, compilationUnit)
+import Language.Java.Syntax(CompilationUnit)
+import Language.Java.Pretty(pretty)
+import Text.Parsec.Error(ParseError)
+
 
 main = defaultMain unitTests
 
 
 unitTests = testGroup "Unit tests"
-  [ testCase "Sanity" $ assertEqual [] (1 :: Integer) 1
+  [ verifyUnchangedByRoundtrip "Roundtrip empty class" "class X\n{\n}"
   ]
 
+
+parse :: String -> [Either ParseError CompilationUnit]
+parse code = parser compilationUnit <$> [code]
+
+
+verifyUnchangedByRoundtrip desc code =
+    testCase desc $ case head (parse code) of
+        Left e -> fail (show e)
+        Right c -> assertEqual [] code $ show $ pretty c
